1769fac3e60f91ddacec36b0432da3032398ae6e
Shovon Hasan <im.shovonhasan@gmail.com>
2023-04-22 20:51:52 -0700

    Configure Github OAuth ids.

--- apps/web/src/app/api/auth/[...nextauth].ts
+++ apps/web/src/app/api/auth/[...nextauth].ts
@@ -2,13 +2,13 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { z } from "zod";
 
-const GITHUB_CLIENT_ID = z.string().parse(process.env.GITHUB_CLIENT_ID);
+const GITHUB_ID = z.string().parse(process.env.GITHUB_ID);
 const GITHUB_SECRET = z.string().parse(process.env.GITHUB_SECRET);
 
 const handler = NextAuth({
   providers: [
     GithubProvider({
-      clientId: GITHUB_CLIENT_ID,
+      clientId: GITHUB_ID,
       clientSecret: GITHUB_SECRET,
     }),
   ],
