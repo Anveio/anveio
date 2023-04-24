b423207f003b781b4c6221d02acf1e16a88d534f
Shovon Hasan <im.shovonhasan@gmail.com>
2023-04-24 09:10:00 -0700

    Fix build issue.

--- apps/web/src/components/Logo.tsx
+++ apps/web/src/components/Logo.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import CompanyLogoWhite from "../../public/company-logo.svg";
 import Image from "next/image";
 
-export function Logo(props: React.ComponentProps<typeof Image>) {
+export function Logo(props: Partial<React.ComponentProps<typeof Image>>) {
   return <Image {...props} src={CompanyLogoWhite} alt="" />;
 }
