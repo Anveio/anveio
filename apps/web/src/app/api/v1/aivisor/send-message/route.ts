475579fd75282ae253acc710d5a8fe9cacc36e44
Shovon Hasan <im.shovonhasan@gmail.com>
2023-04-25 16:43:41 -0700

    NextAuth doesn't work on edge either

--- apps/web/src/app/api/v1/aivisor/send-message/route.ts
+++ apps/web/src/app/api/v1/aivisor/send-message/route.ts
@@ -1,19 +1,11 @@
 import { OpenAIEdgeClient } from "@/lib/features/ai/openai/edge-client";
 import { OPEN_AI_SECRET } from "@/lib/features/ai/openai/openAi";
-import { NEXT_AUTH_HANDLER_OPTIONS } from "@/lib/features/next-auth";
-import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
   try {
-    const session = await getServerSession(NEXT_AUTH_HANDLER_OPTIONS);
-
-    if (!session) {
-      return new NextResponse(new ReadableStream());
-    }
-
     const responseStream = await OpenAIEdgeClient(
       "chat",
       {
