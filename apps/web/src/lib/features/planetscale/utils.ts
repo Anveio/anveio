8c913a8742a06c54887aae4e411a7d3fa78cc4fb
Shovon Hasan <im.shovonhasan@gmail.com>
2023-04-25 16:40:42 -0700

    Remove argon2 cause it doesn't work on edge.

--- apps/web/src/lib/features/planetscale/utils.ts
+++ apps/web/src/lib/features/planetscale/utils.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 import { GLOBAL_DATABASE_CONNECTION } from "./planetscale";
 import { Transaction } from "@planetscale/database";
-import argon2 from "argon2";
 
 export const getDoesUserAlreadyExist = async (
   email?: string | null,
@@ -26,15 +25,6 @@ export const getDoesUserAlreadyExist = async (
   const queryResult = await userAlreadyExistsQuery;
 
   return queryResult.rows.length > 0;
-};
-
-const generatePasswordHash = async (password: string) => {
-  const hashedPassword = await argon2.hash(password, {
-    salt: Buffer.from(
-      "b5108428bd29d1c4a6ec2555e9473db2f381139979b9d9107a16ad83171d3659"
-    ),
-  });
-  return hashedPassword;
 };
 
 export const getUserByEmailAddress = async (
@@ -172,30 +162,6 @@ export const createUserWithOAuthToken = async (
       );
     }
   });
-};
-
-export const createOauthTokenForUserByEmail = (
-  email: string,
-  oauthProviderName: string,
-  access_token: string
-) => {};
-
-export const checkIfPasswordIsValidForUserByEmail = async (
-  email: string,
-  password: string,
-  tx?: Transaction
-) => {
-  const validatedEmail = z.string().email().parse(email);
-  const { rows } = await (tx || GLOBAL_DATABASE_CONNECTION).execute(
-    `SELECT password_hash FROM users WHERE email = ? LIMIT 1`,
-    [validatedEmail]
-  );
-
-  const passwordHashField: unknown = rows[0].at(0) as unknown;
-
-  const password_hash = z.string().parse(passwordHashField);
-
-  return argon2.verify(password_hash, password);
 };
 
 export const createMessageRow = async (userId: string) => {
