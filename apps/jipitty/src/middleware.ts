4d03fe86a0e8a3d393b5731391710383031c1f56
Shovon Hasan <im.shovonhasan@gmail.com>
2023-05-23 22:34:08 -0700

    Define some public routes I guess.

--- apps/jipitty/src/middleware.ts
+++ apps/jipitty/src/middleware.ts
@@ -1,7 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs"
 
-export default authMiddleware()
+export default authMiddleware({
+	publicRoutes: ["/", "/sign-in", "/sign-up", "/login"]
+})
 
 export const config = {
-	matcher: ["/((?!.*\\..*|_next).*)"]
+	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
 }
