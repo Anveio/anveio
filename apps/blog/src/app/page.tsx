c073145f80e89bb5d4742ddc88cbe6a39ba4c83c
Shovon Hasan <im.shovonhasan@gmail.com>
2023-09-03 08:58:06 -0700

    Fix z index issue.

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -7,27 +7,29 @@ import React, { useId } from "react";
 
 export default function Home() {
   return (
-    <main className="min-h-screen antialiased bg-background overflow-hidden relative">
-      <div className="pt-28 md:pt-52 pb-10 md:pb-24 px-8 relative z-40">
-        <div className="max-w-7xl mx-auto flex flex-col items-center">
-          <div className="z-50 opacity-100">
-            <div className="w-full flex justify-center">
-              <div className="py-1 flex items-center space-x-2 border rounded-full border-[#8C8C8C]/[0.4] w-fit px-4 bg-gradient-to-b from-[#8C8C8C]/[0.4] to-[#8C8C8C]/[0.25] shadow-[0px_1px_4px_0px_rgba(255,255,255,.12)]) mb-8">
-                <span className="text-neutral-200">{getSaying(0)}</span>
+    <>
+      <main className="min-h-screen antialiased bg-background overflow-hidden relative">
+        <div className="pt-28 md:pt-52 pb-10 md:pb-24 px-8 relative z-40">
+          <div className="max-w-7xl mx-auto flex flex-col items-center">
+            <div className="z-50 opacity-100">
+              <div className="w-full flex justify-center">
+                <div className="py-1 flex items-center space-x-2 border rounded-full border-[#8C8C8C]/[0.4] w-fit px-4 bg-gradient-to-b from-[#8C8C8C]/[0.4] to-[#8C8C8C]/[0.25] shadow-[0px_1px_4px_0px_rgba(255,255,255,.12)]) mb-8">
+                  <span className="text-neutral-200">{getSaying(0)}</span>
+                </div>
               </div>
             </div>
-          </div>
-          <h1 className="text-white text-center text-3xl md:text-6xl mb-4 font-bold">
-            Shovon Hasan
-          </h1>
-          <div>
-            <p className="text-center font-medium text-base md:text-lg text-[#FFFFFF]/[.48] mb-8"></p>
+            <h1 className="text-white text-center text-3xl md:text-6xl mb-4 font-bold">
+              Shovon Hasan
+            </h1>
+            <div>
+              <p className="text-center font-medium text-base md:text-lg text-[#FFFFFF]/[.48] mb-8"></p>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
       <WithRoom roomId="blog-home">
         <CollaborativeApp />
       </WithRoom>
-    </main>
+    </>
   );
 }
