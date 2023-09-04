cfe441bb3be57044c429d114192c3ce3de7aeac0
Shovon Hasan <im.shovonhasan@gmail.com>
2023-09-03 18:14:42 -0700

    Add challenges.cloudflare.com to CSP.

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -56,7 +56,7 @@ const nextConfig = {
 // https://nextjs.org/docs/advanced-features/security-headers
 const ContentSecurityPolicy = `
     default-src 'self' vercel.live ;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' blob: cdn.vercel-insights.com vercel.live ${
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' blob: cdn.vercel-insights.com vercel.live challenges.cloudflare.com ${
       process.env.NODE_ENV === "development"
         ? "*.clerk.accounts.dev"
         : "clerk.shovonhasan.com"
