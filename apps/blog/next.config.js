da24427c34d73c0d8b1ae411a71e4016e21d87eb
Shovon Hasan <im.shovonhasan@gmail.com>
2023-10-26 08:47:49 -0700

    stuff

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -99,6 +99,21 @@ const securityHeaders = [
     key: "Permissions-Policy",
     value: "camera=(), microphone=(), geolocation=()",
   },
+  {
+    key: "Access-Control-Allow-Credentials",
+    value: "true",
+  },
+  { key: "Access-Control-Allow-Origin", value: "*.shovonhasan.com" },
+  { key: "Access-Control-Allow-Origin", value: "*.anveio.com" },
+  {
+    key: "Access-Control-Allow-Methods",
+    value: "GET,DELETE,PATCH,POST,PUT",
+  },
+  {
+    key: "Access-Control-Allow-Headers",
+    value:
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  },
 ];
 
 module.exports = nextConfig;
