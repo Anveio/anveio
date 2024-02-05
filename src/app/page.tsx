ad22b77df860e376f94610d4fff5550f72229e0e
Shovon Hasan <im.shovonhasan@gmail.com>
2024-02-05 12:30:45 -0800

    Implement scene.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
+"use client";
+
 import { RecordEventOnMount } from "@/lib/analytics/analytics.client";
 import { LiveBlogPostCard } from "@/components/custom/LiveBlogPostCard";
 import { AnalyticsEvent } from "@/lib/analytics/types";
 import { BLOG_POSTS } from "@/lib/blog/posts";
+import dynamic from "next/dynamic";
 
 const posts = Object.values(BLOG_POSTS);
+
+const Scene = dynamic(() => import("@/components/3d/Scene"), { ssr: false });
+
+const View = dynamic(
+  () => import("@react-three/drei").then((mod) => mod.View),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="dark:text-white flex h-96 w-full flex-col items-center justify-center">
+        <svg
+          style={{ width: 40, height: 40 }}
+          className="animate-spin "
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          />
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          />
+        </svg>
+      </div>
+    ),
+  }
+);
 
 const postsToRender = posts
   .filter((post) => {
@@ -34,7 +70,11 @@ export default function Home() {
   return (
     <>
       <main className="">
-        <div className="h-44"></div>
+        <div className="h-96">
+          {" "}
+          <Scene>{/* @ts-expect-error */}</Scene>
+          <View />
+        </div>
         <section className="space-y-4">
           <h1 className="text-center dark:text-gray-400 text-gray-700">
             Featured Essay
